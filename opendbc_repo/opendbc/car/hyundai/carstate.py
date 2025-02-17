8e4321dc9296fecd4f5824a2a9e3fce7de14b950
crwusiz <crwusiz@gmail.com>
2025-02-17 10:20:09 +0900

    fix

--- opendbc_repo/opendbc/car/hyundai/carstate.py
+++ opendbc_repo/opendbc/car/hyundai/carstate.py
@@ -420,7 +420,7 @@ class CarState(CarStateBase):
 
     if self.CP.exFlags & HyundaiExFlags.NAVI:
       if self.CP.flags & HyundaiFlags.CAMERA_SCC and self.CP.exFlags & HyundaiExFlags.CCNC.value:
-        ret.exState.navLimitSpeed = cp.vl["ADRV_0x162"]["SPEEDLIMIT"]
+        ret.exState.navLimitSpeed = cp_cam.vl["ADRV_0x162"]["SPEEDLIMIT"]
       else:
         ret.exState.navLimitSpeed = cp.vl["CLUSTER_SPEED_LIMIT"]["SPEED_LIMIT_1"]
 
