f078a7b429fa449e0b5878ca36f419287a0fb688
crwusiz <crwusiz@gmail.com>
2025-05-17 14:59:44 +0900

    update master

--- selfdrive/controls/lib/latcontrol_angle.py
+++ selfdrive/controls/lib/latcontrol_angle.py
@@ -6,10 +6,13 @@ from openpilot.selfdrive.controls.lib.latcontrol import LatControl
 
 STEER_ANGLE_SATURATION_THRESHOLD = 2.5  # Degrees
 
+
 class LatControlAngle(LatControl):
   def __init__(self, CP, CI):
     super().__init__(CP, CI)
-    self.sat_check_min_speed = 5.0
+    self.sat_check_min_speed = 5.
+    self.use_steer_limited_by_controls = CP.brand == "tesla"
+
     # Initialize the filtered curvature to zero (or an appropriate initial value)
     self.filtered_curvature = 0.0
     # Filter coefficient: adjust between 0 (very smooth) and 1 (no filtering)
@@ -31,7 +34,13 @@ class LatControlAngle(LatControl):
       angle_steers_des = math.degrees(VM.get_steer_from_curvature(-self.filtered_curvature, CS.vEgo, params.roll))
       angle_steers_des += params.angleOffsetDeg
 
-    angle_control_saturated = abs(angle_steers_des - CS.steeringAngleDeg) > STEER_ANGLE_SATURATION_THRESHOLD
+    if self.use_steer_limited_by_controls:
+      # these cars' carcontrolers calculate max lateral accel and jerk, so we can rely on carOutput for saturation
+      angle_control_saturated = steer_limited_by_controls
+    else:
+      # for cars which use a method of limiting torque such as a torque signal (Nissan and Toyota)
+      # or relying on EPS (Ford Q3), carOutput does not capture maxing out torque  # TODO: this can be improved
+     angle_control_saturated = abs(angle_steers_des - CS.steeringAngleDeg) > STEER_ANGLE_SATURATION_THRESHOLD
     angle_log.saturated = bool(self._check_saturation(angle_control_saturated, CS, False, curvature_limited))
     angle_log.steeringAngleDeg = float(CS.steeringAngleDeg)
     angle_log.steeringAngleDesiredDeg = float(angle_steers_des)
