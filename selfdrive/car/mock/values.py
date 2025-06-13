9051f160687eb62ba6b8f6ae0d2ff2831da261c2
Saeed59sa <65347537+Saeed59sa@users.noreply.github.com>
2025-06-14 01:22:12 +0400

    Add firmware query config for mock

--- selfdrive/car/mock/values.py
+++ selfdrive/car/mock/values.py
@@ -1,4 +1,8 @@
+from cereal import car
 from openpilot.selfdrive.car import CarSpecs, PlatformConfig, Platforms
+from openpilot.selfdrive.car.fw_query_definitions import FwQueryConfig, Request, StdQueries
+
+Ecu = car.CarParams.Ecu
 
 
 class CAR(Platforms):
@@ -7,3 +11,15 @@ class CAR(Platforms):
     CarSpecs(mass=1700, wheelbase=2.7, steerRatio=13),
     {}
   )
+
+
+FW_QUERY_CONFIG = FwQueryConfig(
+  requests=[
+    Request(
+      [StdQueries.TESTER_PRESENT_REQUEST, StdQueries.UDS_VERSION_REQUEST],
+      [StdQueries.TESTER_PRESENT_RESPONSE, StdQueries.UDS_VERSION_RESPONSE],
+      whitelist_ecus=[Ecu.eps],
+      bus=0,
+    ),
+  ],
+)
