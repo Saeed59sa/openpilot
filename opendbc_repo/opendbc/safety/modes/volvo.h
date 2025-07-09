f9484a7696a65f0c4f5280a769dc461790b231d1
leomonde <gianlucafedele@gmail.com>
2025-07-09 09:26:32 -0300

    fix2

--- opendbc_repo/opendbc/safety/modes/volvo.h
+++ opendbc_repo/opendbc/safety/modes/volvo.h
@@ -113,7 +113,7 @@ static bool volvo_tx_hook(const CANPacket_t *to_send) {
 
   // Longitudinal control
     if (addr == VOLVO_EUCD_FSM3) {
-      int raw_accel = GET_BYTE(to_send, 1) - 126;
+      int raw_accel = GET_BYTE(to_send, 1) * 0.03 - 3.78;
       if (longitudinal_accel_checks(raw_accel, VOLVO_LONG_LIMITS)) {
         tx = false;
       }
