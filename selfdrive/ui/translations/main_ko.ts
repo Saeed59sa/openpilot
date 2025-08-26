b5f1b7e2256d48521375629acac40427c94fc54b
crwusiz <crwusiz@gmail.com>
2025-08-27 00:03:03 +0900

    ko

--- selfdrive/ui/translations/main_ko.ts
+++ selfdrive/ui/translations/main_ko.ts
@@ -9,7 +9,7 @@
     </message>
     <message>
         <source>Snooze Update</source>
-        <translation>업데이트 일시 중지</translation>
+        <translation type="vanished">업데이트 일시 중지</translation>
     </message>
     <message>
         <source>Reboot and Update</source>
@@ -314,7 +314,7 @@
     </message>
     <message>
         <source>Realdata Files Upload</source>
-        <translation>주행데이터 업로드</translation>
+        <translation type="vanished">주행데이터 업로드</translation>
     </message>
     <message>
         <source>Openpilot controls cruise on/off, set speed</source>
@@ -327,7 +327,7 @@
     <message>
         <source>Are you sure you want to upload files from this folder?
 </source>
-        <translation>정말로 이 폴더에 파일을 업로드하시겠습니까?
+        <translation type="vanished">정말로 이 폴더에 파일을 업로드하시겠습니까?
 </translation>
     </message>
     <message>
@@ -336,11 +336,11 @@
     </message>
     <message>
         <source>Files does not exist</source>
-        <translation>파일이 존재하지 않습니다</translation>
+        <translation type="vanished">파일이 존재하지 않습니다</translation>
     </message>
     <message>
         <source>Upload failed. Exit code: </source>
-        <translation>업로드 실패. 종료 코드: </translation>
+        <translation type="vanished">업로드 실패. 종료 코드: </translation>
     </message>
     <message>
         <source>CameraSCC</source>
@@ -353,6 +353,32 @@
     <message>
         <source>Upload</source>
         <translation>업로드</translation>
+    </message>
+    <message>
+        <source>Realdata Routes Upload</source>
+        <translation>주행 구간 업로드</translation>
+    </message>
+    <message>
+        <source>Routes do not exist</source>
+        <translation>주행 구간이 없습니다</translation>
+    </message>
+    <message>
+        <source>Select Route to Upload</source>
+        <translation>선택한 주행 구간 업로드</translation>
+    </message>
+    <message>
+        <source>Upload route: %1
+Segments: %2
+Are you sure?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Upload completed successfully</source>
+        <translation>업로드 성공</translation>
+    </message>
+    <message>
+        <source>Upload failed</source>
+        <translation>업로드 실패</translation>
     </message>
 </context>
 <context>
@@ -658,7 +684,7 @@ Firehose Mode allows you to maximize your training data uploads to improve openp
     </message>
     <message>
         <source>MAX</source>
-        <translation>MAX</translation>
+        <translation type="vanished">MAX</translation>
     </message>
 </context>
 <context>
@@ -879,11 +905,11 @@ Firehose Mode allows you to maximize your training data uploads to improve openp
     <name>QObject</name>
     <message>
         <source>Reboot</source>
-        <translation>재부팅</translation>
+        <translation type="vanished">재부팅</translation>
     </message>
     <message>
         <source>Exit</source>
-        <translation>종료</translation>
+        <translation type="vanished">종료</translation>
     </message>
     <message>
         <source>openpilot</source>
@@ -974,105 +1000,105 @@ Firehose Mode allows you to maximize your training data uploads to improve openp
     <name>Setup</name>
     <message>
         <source>WARNING: Low Voltage</source>
-        <translation>경고: 전압이 낮습니다</translation>
+        <translation type="vanished">경고: 전압이 낮습니다</translation>
     </message>
     <message>
         <source>Power your device in a car with a harness or proceed at your own risk.</source>
-        <translation>장치를 하네스를 통해 차량 전원에 연결하세요. USB 전원에서는 예상치 못한 문제가 생길 수 있습니다.</translation>
+        <translation type="vanished">장치를 하네스를 통해 차량 전원에 연결하세요. USB 전원에서는 예상치 못한 문제가 생길 수 있습니다.</translation>
     </message>
     <message>
         <source>Power off</source>
-        <translation>전원 끄기</translation>
+        <translation type="vanished">전원 끄기</translation>
     </message>
     <message>
         <source>Continue</source>
-        <translation>계속</translation>
+        <translation type="vanished">계속</translation>
     </message>
     <message>
         <source>Getting Started</source>
-        <translation>시작하기</translation>
+        <translation type="vanished">시작하기</translation>
     </message>
     <message>
         <source>Before we get on the road, let’s finish installation and cover some details.</source>
-        <translation>출발 전 설정을 완료하고 세부 사항을 살펴봅니다.</translation>
+        <translation type="vanished">출발 전 설정을 완료하고 세부 사항을 살펴봅니다.</translation>
     </message>
     <message>
         <source>Connect to Wi-Fi</source>
-        <translation>Wi-Fi 연결</translation>
+        <translation type="vanished">Wi-Fi 연결</translation>
     </message>
     <message>
         <source>Back</source>
-        <translation>뒤로</translation>
+        <translation type="vanished">뒤로</translation>
     </message>
     <message>
         <source>Continue without Wi-Fi</source>
-        <translation>Wi-Fi 연결 없이 진행</translation>
+        <translation type="vanished">Wi-Fi 연결 없이 진행</translation>
     </message>
     <message>
         <source>Waiting for internet</source>
-        <translation>인터넷 연결 대기 중</translation>
+        <translation type="vanished">인터넷 연결 대기 중</translation>
     </message>
     <message>
         <source>Enter URL</source>
-        <translation>URL 입력</translation>
+        <translation type="vanished">URL 입력</translation>
     </message>
     <message>
         <source>for Custom Software</source>
-        <translation>커스텀 소프트웨어</translation>
+        <translation type="vanished">커스텀 소프트웨어</translation>
     </message>
     <message>
         <source>Downloading...</source>
-        <translation>다운로드 중...</translation>
+        <translation type="vanished">다운로드 중...</translation>
     </message>
     <message>
         <source>Download Failed</source>
-        <translation>다운로드 실패</translation>
+        <translation type="vanished">다운로드 실패</translation>
     </message>
     <message>
         <source>Ensure the entered URL is valid, and the device’s internet connection is good.</source>
-        <translation>입력된 URL이 유효하고 인터넷 연결이 원활한지 확인하세요.</translation>
+        <translation type="vanished">입력된 URL이 유효하고 인터넷 연결이 원활한지 확인하세요.</translation>
     </message>
     <message>
         <source>Reboot device</source>
-        <translation>장치 재부팅</translation>
+        <translation type="vanished">장치 재부팅</translation>
     </message>
     <message>
         <source>Start over</source>
-        <translation>다시 시작</translation>
+        <translation type="vanished">다시 시작</translation>
     </message>
     <message>
         <source>Something went wrong. Reboot the device.</source>
-        <translation>문제가 발생했습니다. 장치를 재부팅하세요.</translation>
+        <translation type="vanished">문제가 발생했습니다. 장치를 재부팅하세요.</translation>
     </message>
     <message>
         <source>No custom software found at this URL.</source>
-        <translation>이 URL에서 커스텀 소프트웨어를 찾을 수 없습니다.</translation>
+        <translation type="vanished">이 URL에서 커스텀 소프트웨어를 찾을 수 없습니다.</translation>
     </message>
     <message>
         <source>Select a language</source>
-        <translation>언어를 선택하세요</translation>
+        <translation type="vanished">언어를 선택하세요</translation>
     </message>
     <message>
         <source>Choose Software to Install</source>
-        <translation>설치할 소프트웨어 선택</translation>
+        <translation type="vanished">설치할 소프트웨어 선택</translation>
     </message>
     <message>
         <source>openpilot</source>
-        <translation>오픈파일럿</translation>
+        <translation type="vanished">오픈파일럿</translation>
     </message>
     <message>
         <source>Custom Software</source>
-        <translation>커스텀 소프트웨어</translation>
+        <translation type="vanished">커스텀 소프트웨어</translation>
     </message>
     <message>
         <source>WARNING: Custom Software</source>
-        <translation>경고: 커스텀 소프트웨어</translation>
+        <translation type="vanished">경고: 커스텀 소프트웨어</translation>
     </message>
     <message>
         <source>Use caution when installing third-party software. Third-party software has not been tested by comma, and may cause damage to your device and/or vehicle.
 
 If you&apos;d like to proceed, use https://flash.comma.ai to restore your device to a factory state later.</source>
-        <translation>타사 소프트웨어를 설치할 때는 주의하십시오. 타사 소프트웨어는 comma에 의해 테스트되지 않았으며 장치나 차량에 손상을 줄 수 있습니다.
+        <translation type="vanished">타사 소프트웨어를 설치할 때는 주의하십시오. 타사 소프트웨어는 comma에 의해 테스트되지 않았으며 장치나 차량에 손상을 줄 수 있습니다.
 
 진행하려면 https://flash.comma.ai를 사용하여 나중에 장치를 공장 초기화하세요.</translation>
     </message>
@@ -1116,15 +1142,15 @@ If you&apos;d like to proceed, use https://flash.comma.ai to restore your device
     </message>
     <message>
         <source>HIGH</source>
-        <translation>높음</translation>
+        <translation type="vanished">높음</translation>
     </message>
     <message>
         <source>GOOD</source>
-        <translation>좋음</translation>
+        <translation type="vanished">좋음</translation>
     </message>
     <message>
         <source>OK</source>
-        <translation>OK</translation>
+        <translation type="vanished">OK</translation>
     </message>
     <message>
         <source>VEHICLE</source>
@@ -1427,7 +1453,7 @@ If you&apos;d like to proceed, use https://flash.comma.ai to restore your device
     </message>
     <message>
         <source>Use the openpilot system for adaptive cruise control and lane keep driver assistance. Your attention is required at all times to use this feature.</source>
-        <translation>ACC 및 차선 유지 지원을 위해 오픈파일럿 시스템을 사용하십시오. 이 기능을 사용하려면 항상주의를 기울여야합니다.</translation>
+        <translation type="vanished">ACC 및 차선 유지 지원을 위해 오픈파일럿 시스템을 사용하십시오. 이 기능을 사용하려면 항상주의를 기울여야합니다.</translation>
     </message>
     <message>
         <source> Changing this setting will restart openpilot if the car is powered on.</source>
@@ -1443,15 +1469,19 @@ If you&apos;d like to proceed, use https://flash.comma.ai to restore your device
     </message>
     <message>
         <source>Record Audio Feedback with LKAS button</source>
-        <translation>LKAS 버튼으로 오디오 피드백 기록</translation>
+        <translation type="vanished">LKAS 버튼으로 오디오 피드백 기록</translation>
     </message>
     <message>
         <source>Press the LKAS button to record and share driving feedback with the openpilot team. When this toggle is disabled, the button acts as a bookmark button. The event will be highlighted in comma connect and the segment will be preserved on your device&apos;s storage.
 
 Note that this feature is only compatible with select cars.</source>
-        <translation>LKAS 버튼을 눌러 오픈파일럿팀과 주행 피드백을 기록하고 공유하세요. 이 토글이 비활성화되면 버튼은 북마크 버튼 역할을합니다. 이벤트는 Comma Connect에서 강조 표시되며 주행 구간은 장치의 스토리지에 보존됩니다.
+        <translation type="vanished">LKAS 버튼을 눌러 오픈파일럿팀과 주행 피드백을 기록하고 공유하세요. 이 토글이 비활성화되면 버튼은 북마크 버튼 역할을합니다. 이벤트는 Comma Connect에서 강조 표시되며 주행 구간은 장치의 스토리지에 보존됩니다.
 
 이 기능은 일부 차량만 호환됩니다</translation>
+    </message>
+    <message>
+        <source>Use the openpilot system for adaptive cruise control and lane keep driver assistance. Your attention is required at all times to use this feature. Changing this setting takes effect when the car is powered off.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
